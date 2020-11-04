64848fd237a379458bdae979628611d35de4cc42
pabloPeralta <paps21.gol@gmail.com>
2020-11-04 15:52:44 -0600

    add style in grid and cards

--- pages/personaje/[id].js
+++ pages/personaje/[id].js
@@ -1,15 +1,71 @@
 import axios from 'axios';
-import { useReouter } from 'next/router';
-import { useState } from 'react';
+import { useRouter } from "next/router";
+import Link from 'next/link';
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const Detail =  ( props ) =>  {
-	const router = useReouter()
+	const router = useRouter()
 	const { id } = router.query
 	
 	return (
-		<>
+		<div className="container">
+			<main>
+
+			<Link href={'/'}><a> <FontAwesomeIcon icon={faHome} /></a></Link>
 			<h1>Perfil</h1>
-			<h2>{props.name}</h2>
-		</>
+			<div className="card">
+				<img src={props.image}/>
+				<h2>gender: {props.gender}</h2>
+    		<h2>specie:{props.species}</h2>
+				<h2>name: {props.name}</h2>
+			</div>
+    	
+			</main>
+
+			<style jsx>{`
+					.container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+				main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+				.card {
+          margin: 20px;
+          flex-basis: 45%;
+          padding: 10px;
+          text-align: center;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+          -webkit-box-shadow: 1px 3px 9px 3px rgba(0,0,0,0.48); 
+          box-shadow: 1px 3px 9px 3px rgba(0,0,0,0.48);
+        }
+			`}</style>
+		</div>
 	)
 }
+
+Detail.getInitialProps = async (ctx) => {
+	console.log(ctx.query.id);
+
+	const res = await axios.get(`https://rickandmortyapi.com/api/character/${ctx.query.id}`);
+	const uniqueDetail = res.data
+
+	return uniqueDetail
+}
+
+export default Detail
